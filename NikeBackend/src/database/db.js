bdffb9cb96e978ad9c0e8113fc4b997347be6691
oltion <oltionelezi17@gmail.com>
2023-05-02 03:50:51 +0200

    create Order API intergration

--- NikeBackend/src/database/db.js
+++ NikeBackend/src/database/db.js
@@ -2,11 +2,21 @@ const { MongoClient } = require("mongodb");
 const uri =
   "mongodb+srv://oltionelezi17:go2UYR9XfUVpZki5@cluster0.dvnz6mb.mongodb.net/?retryWrites=true&w=majority";
 
-const client = new MongoClient(uri);
+let client;
 
-const database = client.db("test");
-const products = database.collection("products");
+const getDB = () => {
+  if (!client) {
+    client = new MongoClient(uri);
+  }
 
-module.exports = {
-  products,
+  const database = client.db("test");
+  const products = database.collection("products");
+  const orders = database.collection("orders");
+
+  return {
+    products,
+    orders,
+  };
 };
+
+module.exports = getDB;
